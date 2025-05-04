7ad88b055d17d44ef8d334f302500185c28aa3d6
Daniel David Gonzalez <danieldavidgonzalez8@gmail.com>
2025-05-04 15:50:24 -0400

    chore: update mock for production

--- sync-song-frontend/src/constants/mock-data.ts
+++ sync-song-frontend/src/constants/mock-data.ts
@@ -1,3 +1,5 @@
+const env = process.env.NODE_ENV || "development";
+
 // Datos de ejemplo
 const mockSongs = [
   {
@@ -5,16 +7,20 @@ const mockSongs = [
     title: "Riri",
     artist: "Young Miko",
     cover: "/placeholder.svg?height=40&width=40",
-    // src: "https://7baz7ly5c7.ufs.sh/f/O3tllwcIS2Rubf9oQB4ndmVcuqPbSWgph3e0tDK24izoZXs8",
-    src: "/music/Young Miko - Riri.mp3",
+    src:
+      env === "production"
+        ? "https://7baz7ly5c7.ufs.sh/f/O3tllwcIS2Rubf9oQB4ndmVcuqPbSWgph3e0tDK24izoZXs8"
+        : "/music/Young Miko - Riri.mp3",
   },
   {
     id: 2,
     title: "Lisa",
     artist: "Young Miko",
     cover: "/placeholder.svg?height=40&width=40",
-    // src: "https://7baz7ly5c7.ufs.sh/f/O3tllwcIS2Ru4cBQD1OqiJltO0RInd9fK8bgMXQA1WkcT4Hj",
-    src: "/music/Young Miko - Lisa.mp3",
+    src:
+      env === "production"
+        ? "https://7baz7ly5c7.ufs.sh/f/O3tllwcIS2Ru4cBQD1OqiJltO0RInd9fK8bgMXQA1WkcT4Hj"
+        : "/music/Young Miko - Lisa.mp3",
   },
 ];
 
